c34523c89392a45e70254f57919ba7674c0cb047
sxh705 <2362989228@qq.com>
2022-10-01 16:30:58 +0800

    Changes

--- src/com/level/game/xstl.java
+++
@@ -1,7 +0,0 @@
-package com.level.game;
-
-public class xstl {
-    static double xrand(int l,int r){
-        return (l + Math.random()*(r - l + 1));
-    }
-}
--- src/com/sxh/排行榜/排行榜_子实体.java
+++
@@ -1,4 +0,0 @@
-package com.sxh.排行榜;
-
-public class 排行榜_子实体 {
-}
